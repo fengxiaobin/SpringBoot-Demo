4ee56177e166fd3c7a08c333da8a50740430d2e3
fengxiaobin <fengxiaobin_umpay@163.com>
2019-11-19 15:27:19 +0800

    更新developer方法

--- src/main/java/com/travelsky/demo/TestGit.java
+++ src/main/java/com/travelsky/demo/TestGit.java
@@ -4,8 +4,5 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello GitHub !");
     }
-    
-    public static void hello(String[] args) {
-        System.out.println("Hello GitHub !");
-    }
+
 }
